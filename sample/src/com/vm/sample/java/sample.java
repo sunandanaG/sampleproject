c6acf587d9a6db616bc92284bc0b2d76df47299b
Doresh Ghanta <Doresh Ghanta@Doresh-Desktop>
2021-05-30 17:12:42 +0530

    sample commit

---
+++ sample/src/com/vm/sample/java/sample.java
@@ -0,0 +1,12 @@
+package com.vm.sample.java;
+
+public class sample {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+
+	}
+
+}
